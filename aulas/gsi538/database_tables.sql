4d0a9023bbe0c3eb0681818ad4139c3ce6caf729
eduardocunha11 <eduardocunha11@gmail.com>
2019-08-21 13:49:22 -0300

    retirei a pasta gwt-unit cache das apps GWT

--- aulas/gsi538/database_tables.sql
+++ aulas/gsi538/database_tables.sql
@@ -12,10 +12,9 @@ MySQL - 8.0.14 : Database - test
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-
-USE `test`;
-
+CREATE DATABASE garagem;
+USE garagem;
+
 /*Table structure for table `car_brands` */
 
 DROP TABLE IF EXISTS `car_brands`;
