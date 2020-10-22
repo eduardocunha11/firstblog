8b36b5e5f85eeb7cda9b32c347fec4a52a0b76e5
Eduardo Campos <eduardocunha11@gmail.com>
2020-10-22 17:58:21 -0300

    Projeto para lidar com Data e Hora em java

---
+++ workspace/ExemploDataHora/src/ConversaoDatas.java
@@ -0,0 +1,37 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ConversaoDatas {
+	
+	public static void main(String[] args) {
+		Calendar calendar = Calendar.getInstance();
+		DateFormat formatter = DateFormat.getDateInstance();
+		
+		Date data2;
+		try {
+			data2 = formatter.parse("12/01/1995");
+//			System.out.println(data2);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		Date data = calendar.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		System.out.println("Data formatada: "+sdf.format(data));
+		
+		//Converte Objetos
+		Date data3;
+		try {
+			data3 = sdf.parse("02/08/1970");
+			System.out.println("Data convertida: " + data3);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ workspace/ExemploDataHora/src/Principal.java
@@ -0,0 +1,67 @@
+import java.util.Calendar;
+
+public class Principal {
+	
+	public static void main(String[] args) {		
+			
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.YEAR, 2020);
+		c.set(Calendar.MONTH, Calendar.OCTOBER);
+		c.set(Calendar.DAY_OF_MONTH, 22);
+	
+		c.set(Calendar.HOUR_OF_DAY, 15);
+		c.set(Calendar.MINUTE, 30);
+		c.set(Calendar.MILLISECOND, 0);
+
+		System.out.println("Data/Hora atual: "+c.getTime());
+		System.out.println("Ano: "+c.get(Calendar.YEAR));
+		System.out.println("Mês: "+c.get(Calendar.MONTH));
+		System.out.println("Dia do Mês: "+c.get(Calendar.DAY_OF_MONTH));
+		
+			
+		/**
+		 * Cria um objeto Calendar contendo o dia e hora atual
+		 */
+		
+		Calendar c2 = Calendar.getInstance();
+		c2.set(Calendar.YEAR, 2020);
+		c2.set(Calendar.MONTH, Calendar.OCTOBER);
+		c2.set(Calendar.DAY_OF_MONTH, 22);
+
+		System.out.println("Data/Hora atual: "+c2.getTime());
+		System.out.println("Ano: "+c2.get(Calendar.YEAR));
+		System.out.println("Mês: "+c2.get(Calendar.MONTH));
+		System.out.println("Dia do Mês: "+c2.get(Calendar.DAY_OF_MONTH));
+		
+		
+		if (c2.after(c)) {
+			System.out.println("Calendar 2 vem depois do Calendar 1 no tempo...");
+		}
+		
+		/**
+		 * Calcula a duração da chamada
+		 * 1 minuto = 60 segundos = 60000 milissegundos
+		 */
+		
+		long duration = (c2.getTimeInMillis() - c.getTimeInMillis()) / 60000;
+		System.out.println("Intervalo: " + duration + " minutos");
+		
+		
+		/**
+		 * Outro exemplo: Recupera a hora do dia e verifica qual deve ser a msg de saudação
+		 */
+		
+		Calendar c1 = Calendar.getInstance();
+		int hora = c1.get(Calendar.HOUR_OF_DAY);
+		
+		System.out.println("hora: " + hora);
+
+		if(hora >= 6 && hora < 12){
+			System.out.println("Bom Dia");
+		} else if (hora >= 12 && hora <= 18){
+			System.out.println("Boa Tarde");
+		} else {
+			System.out.println("Boa Noite");
+		}
+	}		
+}
