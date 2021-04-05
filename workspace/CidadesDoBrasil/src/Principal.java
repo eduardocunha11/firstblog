9e6ff90789ce77ef2af7bbb91fd41688136b4057
Eduardo Campos <eduardocunha11@gmail.com>
2021-04-05 09:07:18 -0300

    atualizei o projeto que ensina usar HashMap em Java

--- workspace/CidadesDoBrasil/src/Principal.java
+++ workspace/CidadesDoBrasil/src/Principal.java
@@ -3,17 +3,56 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Scanner;
 
 public class Principal {
-	
-	
-	private static HashMap<String, Aluno> alunosPC2 = new HashMap<String, Aluno>();
-	
+		
 	private static HashMap<String, String> estadosCapitaisMap = new HashMap<String, String>();
 	private static HashMap<String, List<String>> cidadesEstadoMap = new HashMap<String, List<String>>();
 	
+	private static HashMap<String, List<Cidade>> cidadesEstadosInfoMap = new HashMap<String, List<Cidade>>();
+	
 	public static void main(String[] args) {
+		
+		
+//		cadastrarCidadeNoEstado(cidadesEstadoMap, "SP");
 
+		// Coloca no mapa, algumas cidades de Minas Gerais 
+		
+		List<Cidade> listaCidadesMG = new ArrayList<Cidade>();
+		
+		List<String> bairrosBetim = new ArrayList<String>();
+		bairrosBetim.add("Capelinha");
+		bairrosBetim.add("Açude");
+		
+		Cidade betim = new Cidade();
+		betim.setNome("Betim");
+		betim.setIDH(0.749);
+		betim.setPopulacao(439340);
+		betim.setBairros(bairrosBetim);
+		
+		listaCidadesMG.add(betim);
+		
+		
+		cidadesEstadosInfoMap.put("MG", listaCidadesMG);
+		
+		List<String> bairrosNovaLima = new ArrayList<String>();
+		bairrosNovaLima.add("Centro");
+		bairrosNovaLima.add("Vila da Serra");
+		bairrosNovaLima.add("Vale do Sereno");
+		
+	
+		Cidade novaLima = new Cidade();
+		novaLima.setNome("Nova Lima");
+		novaLima.setIDH(0.813);
+		novaLima.setPopulacao(93577);
+		novaLima.setBairros(bairrosNovaLima);
+		
+		
+		cidadesEstadosInfoMap.get("MG").add(novaLima);
+		
+		
+		
 		// Coloca no mapa, algumas capitais de cada estado brasileiro
 		estadosCapitaisMap.put("Minas Gerais", "Belo Horizonte");
 		estadosCapitaisMap.put("Goiás", "Goiânia");
@@ -21,12 +60,12 @@ public class Principal {
 		estadosCapitaisMap.put("Rio Grande do Norte", "Natal");
 		estadosCapitaisMap.put("Alagoas", "Maceió");
 		estadosCapitaisMap.put("São Paulo", "São Paulo");
+		estadosCapitaisMap.put("Tocantis", "Palmas");
 
 	
-		System.out.println("A capital de Minas Gerais eh: " + estadosCapitaisMap.get("Minas Gerais"));
+//		System.out.println("A capital de Minas Gerais eh: " + estadosCapitaisMap.get("Minas Gerais"));
 		
 		List<String> cidadesDeMinasGeraisList = new ArrayList<String>();
-		cidadesDeMinasGeraisList.add("Ouro Preto");
 		cidadesDeMinasGeraisList.add("Betim");
 		cidadesDeMinasGeraisList.add("Contagem");
 		cidadesDeMinasGeraisList.add("Nova Lima");
@@ -34,7 +73,7 @@ public class Principal {
 		cidadesDeMinasGeraisList.add("Uberaba");
 		cidadesDeMinasGeraisList.add("Araguari");
 		
-		System.out.println("O tamanho da lista de cidades de MG eh: " + cidadesDeMinasGeraisList.size());
+//		System.out.println("O tamanho da lista de cidades de MG eh: " + cidadesDeMinasGeraisList.size());
 		
 		
 		List<String> cidadesDeGoiasList = new ArrayList<String>();
@@ -60,6 +99,7 @@ public class Principal {
 		
 		
 		cidadesEstadoMap.get("Minas Gerais").add("Ouro Branco");
+		cidadesEstadoMap.get("Minas Gerais").add("Ouro Preto");
 		cidadesEstadoMap.get("Minas Gerais").add("Ribeirão das Neves");
 		
 		cidadesEstadoMap.get("Goiás").add("Buriti Alegre");
@@ -91,6 +131,45 @@ public class Principal {
 			}
 			
 		}
+		
+		// Verificar se a palavra "Ouro" aparece dentro da lista de cidades mineiras
+		// Caso aparecer, imprimir quais são as cidades de MG que possuem esta palavra no nome
+		// Como fazer isso???
+		
+		System.out.println("EXEMPLO DE USO DO METODO CONTAINS()");
+		
+		List<String> cidadesMineiras = cidadesEstadoMap.get("Minas Gerais");
+		Iterator<String> cidadesMineirasIterator = cidadesMineiras.iterator();
+		String cidadeMineira = "";
+		String keyword = "Ouro"; // palavra-chave
+		Integer tamanhoPalavra = 0;
+		
+		while (cidadesMineirasIterator.hasNext()) {
+			cidadeMineira = cidadesMineirasIterator.next();
+			if (cidadeMineira.contains(keyword)) {
+				System.out.println("A cidade " + cidadeMineira + " possui a palavra 'Ouro' no nome...");
+				tamanhoPalavra = cidadeMineira.length();
+				System.out.println("A palavra " + cidadeMineira.substring(5, tamanhoPalavra) + " aparece apos a palavra 'Ouro'");
+			}
+		}
+	} // fim do main
+	
+	
+	public static void cadastrarCidadeNoEstado(HashMap<String, List<String>> mapa, String estado) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String cidadePaulista = "";
+		
+		System.out.println("Digite uma cidade paulista");
+		cidadePaulista = scan.nextLine();
+		
+		List<String> cidadesDeSP = new ArrayList<String>();
+		cidadesDeSP.add(cidadePaulista);
+		
+		mapa.put(estado, cidadesDeSP);
+		
 	}
 
 }
